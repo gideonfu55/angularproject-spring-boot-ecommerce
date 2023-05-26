456daee60f882012ebe81d1e0d03317739439752
jcmonkey87 <fuzhonglong@gmail.com>
2023-05-26 22:08:13 +0800

    update crossorigin

--- src/main/java/com/learnfullstack/springbootecommerce/dao/ProductRepository.java
+++ src/main/java/com/learnfullstack/springbootecommerce/dao/ProductRepository.java
@@ -1,9 +1,11 @@
 package com.learnfullstack.springbootecommerce.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.learnfullstack.springbootecommerce.entity.Product;
 
+@CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long> {
   
 }
