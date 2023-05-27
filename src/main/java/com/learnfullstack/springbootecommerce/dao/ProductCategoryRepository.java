a5f711a49d189876c2f2c126698154f91f6650b4
jcmonkey87 <fuzhonglong@gmail.com>
2023-05-27 22:27:31 +0800

    add CrossOrigin setting for ProductCategoryRepository

--- src/main/java/com/learnfullstack/springbootecommerce/dao/ProductCategoryRepository.java
+++ src/main/java/com/learnfullstack/springbootecommerce/dao/ProductCategoryRepository.java
@@ -2,9 +2,11 @@ package com.learnfullstack.springbootecommerce.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.learnfullstack.springbootecommerce.entity.ProductCategory;
 
+@CrossOrigin("http://localhost:4200")
 @RepositoryRestResource(collectionResourceRel = "productCategory", path = "product-category")
 public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Long> {
   
