e7b48fdec334cedcef44847012ba86391e9ce54a
jcmonkey87 <fuzhonglong@gmail.com>
2023-06-03 19:54:55 +0800

    add CountryRepository

--- src/main/java/com/learnfullstack/springbootecommerce/entity/Country.java
+++ src/main/java/com/learnfullstack/springbootecommerce/entity/Country.java
@@ -2,6 +2,8 @@ package com.learnfullstack.springbootecommerce.entity;
 
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -30,6 +32,7 @@ public class Country {
   private String name;
 
   @OneToMany(mappedBy = "country")
+  @JsonIgnore
   private List<State> states;
   
 }
